8a47b1d49e07cbf17008fa30cf62c94a8fba75d2
Slifer <antosidi64@yahoo.gr>
2021-05-25 17:55:45 +0300

    fafgvafg

--- src/matlab/gmp_test/simulation/orient_gmp_update.m
+++ src/matlab/gmp_test/simulation/orient_gmp_update.m
@@ -62,7 +62,8 @@ for i=1:3, dvRotd_new(i,:)=[diff(vRotd_new(i,:)) 0]/Ts; end
 
 %% Update GMP
 
-x_dot = 1/Timed(end);
+tau = Timed(end);
+x_dot = 1/tau;
 x_ddot = 0;
 
 gmp_o_new = gmp_o.deepCopy();
@@ -86,23 +87,32 @@ x_up = x(up_ind);
 
 for j=1:length(up_ind)
     k = up_ind(j);
+    gmp_up.updatePos(x_up(j), qd_new_data(:,k));
     gmp_up.updateQuat(x_up(j), Qd_new(:,k));
-%     gmp_up.updatePos(x_up(j), qd_new_data(:,k));
 %     gmp_up.updateRotVel(x_up(j), x_dot, vRotd_new(:,k), Qd_new(:,k) );
 %     gmp_up.updateRotAccel(x_up(j), x_dot, x_ddot, dvRotd_new(:,k), vRotd_new(:,k), Qd_new(:,k) );
 end
 
 q_data = zeros(3, n_data);
 Q_data = zeros(4, n_data);
+vRot_data = zeros(3, n_data);
+dvRot_data = zeros(3, n_data);
 
 q_new_data = zeros(3, n_data);
 Q_new_data = zeros(4, n_data);
-for j=1:n_data
-    Q_data(:,j) = gmp_o.getQd(x(j));
+vRot_new_data = zeros(3, n_data);
+dvRot_new_data = zeros(3, n_data);
+
+for j=1:n_data  
     q_data(:,j) = gmp_o.getYd(x(j));
-    
-    Q_new_data(:,j) = gmp_o_new.getQd(x(j));
+    Q_data(:,j) = gmp_o.getQd(x(j));
+    vRot_data(:,j) = gmp_o.getVd(x(j), x_dot);
+    dvRot_data(:,j) = gmp_o.getVdDot(x(j), x_dot, x_ddot);
+
     q_new_data(:,j) = gmp_o_new.getYd(x(j));
+    Q_new_data(:,j) = gmp_o_new.getQd(x(j));
+    vRot_new_data(:,j) = gmp_o_new.getVd(x(j), x_dot);
+    dvRot_new_data(:,j) = gmp_o_new.getVdDot(x(j), x_dot, x_ddot);
 end
 
 
@@ -117,13 +127,14 @@ for i=1:3
    if (i==4), xlabel('time [$s$]', 'interpreter','latex', 'fontsize',15); end
 end
 
-% figure;
-% for i=1:4
-%    subplot(4,1,i); hold on;
-%    plot(Timed, Qd_new(i,:), 'LineWidth',2, 'Color','blue', 'LineStyle','-');
-%    plot(Timed, Q_data(i,:), 'LineWidth',2, 'Color',[0.85 0.33 0.1], 'LineStyle','--');
-%    plot(Timed, Q_new_data(i,:), 'LineWidth',2, 'Color','magenta', 'LineStyle',':');
-%    if (i==1), legend({'$Q_{d,new}$', '$Q$', '$Q_{new}$'}, 'interpreter','latex', 'fontsize',15); end
-%    if (i==4), xlabel('time [$s$]', 'interpreter','latex', 'fontsize',15); end
-% end
+figure;
+for i=1:3
+   subplot(3,1,i); hold on;
+   plot(Timed, vRotd_new(i,:), 'LineWidth',2, 'Color','blue', 'LineStyle','-');
+   plot(Timed, vRot_data(i,:), 'LineWidth',2, 'Color',[0.85 0.33 0.1], 'LineStyle','--');
+   plot(Timed, vRot_new_data(i,:), 'LineWidth',2, 'Color','magenta', 'LineStyle',':');
+   plot(Timed(up_ind), vRotd_new(i,up_ind), 'LineStyle','none', 'Marker','*', 'MarkerSize',10, 'Color','red', 'LineWidth',2, 'HandleVisibility','off');
+   if (i==1), legend({'$q_{d,new}$', '$q$', '$q_{new}$'}, 'interpreter','latex', 'fontsize',15); end
+   if (i==4), xlabel('time [$s$]', 'interpreter','latex', 'fontsize',15); end
+end
 
